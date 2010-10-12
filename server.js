09ee5a1880ee8dc75502c9d02436c5d088c06b6d
Sri Prasanna <k.sriprasanna@gmail.com>
2010-10-12 23:07:45 +0530

    Adding ejs

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ process.addListener('uncaughtException', function(err, stack) {
   console.log('---------------------');
 });
 
-var LiveStats = require('./lib/livestats');
+var SlideShareLive = require('./lib/slideshare-live');
 
-new LiveStats({
-  port: 8000,
-  geoipServer: {
-    hostname: 'geoip.peepcode.com'
-  , port: 80
-  }
+new SlideShareLive({
+    port: parseInt(process.env.PORT || 8000, 10)
+  , slideshare: {
+      secret: 'LQabSfjq'
+    , key: 'kfqB74Zi'
+    , apiURL: 'http://www.slideshare.net/api/2/'
+    }
 });
